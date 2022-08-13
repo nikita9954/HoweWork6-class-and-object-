75388482f3c7eff3a73ed217cca3d1828118ff70
nikita9954 <kunitsa.nikita2@gmail.com>
2022-08-13 14:44:31 +0300

    создали счет с балансом на счету

---
+++ src/CreditCard.java
@@ -0,0 +1,15 @@
+public class CreditCard {
+    private int bankbnumber;
+    private int money;
+void getnumber (int m){
+    money=m;
+    System.out.println("money on balance "+ money);
+    }
+void getaccount (int c){
+    bankbnumber=c;
+    System.out.println("Card number " + bankbnumber);
+    }
+
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+    CreditCard creditCard=new CreditCard();
+        creditCard.getnumber(scanner.nextInt());
+        creditCard.getaccount(45255);
+    }
+}
